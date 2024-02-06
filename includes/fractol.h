bd10b0b3db1c4af69a573f8a10f2a105d1c14157
Emran Binjamaan <therealemran.design@gmail.com>
2024-02-06 20:42:12 +0400

    Hello, world

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 08:42:35 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/01/24 08:42:55 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/02/06 20:40:17 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,44 @@
 # include "mlx.h"
 # include <math.h>
 
+/*--- CONST. DEFINES ---*/
+
+# ifndef WIN_HEIGHT
+#  define WIN_HEIGHT 960
+# endif // !WIN_HEIGHT
+
+# ifndef WIN_WIDTH
+#  define WIN_WIDTH 1080
+# endif // !WIN_WIDTH
+
+# define RED "\x1b[31m"
+# define GREEN "\x1b[32m"
+# define DFLT "\x1b[0m"
+
+# define NONE 0
+# define JULIA 1
+# define MANDELBROT 2
+
 # ifdef __linux__
 # endif // !__linux__
 
 # ifdef __MACH__
 # endif // !__MACH__
 
+/*--- STRUCTS ---*/
+
+
+typedef struct s_range
+{
+	t_eint	left_bound;
+	t_eint	right_bound;
+}	t_range;
+
+typedef struct s_program
+{
+	void	*mlx;
+	void	*win;
+	t_range	range;
+	bool	error;
+}	t_program;
 #endif // !FRACTOL_H
--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,64 @@
 /*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 08:51:09 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/01/24 08:51:39 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/02/06 20:41:51 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+static int			parse_input(int c, char **v);
+static t_program	init_program(char **v);
+
 int	main(int c, char *v[])
 {
-	(void)c; (void)v;
-	ft_putendl_fd("Hello!", 1);
-	void* mlx_ptr = mlx_init();
-	(void)mlx_ptr;
+	static t_program	program;
+	int					chosen_set;
+
+	chosen_set = parse_input(c, v);
+	if (JULIA == chosen_set)
+		// ft_julia(v);
+		return (ft_putendl_fd("You chose Julia", STDOUT_FILENO), 0);
+	else if (MANDELBROT == chosen_set)
+		// ft_mandelbrot();
+		return (ft_putendl_fd("You chose Mandelbrot", STDOUT_FILENO), 0);
+	else
+		return (ft_putendl_fd(RED"usage: fractol [set_name] [x1] [x2]"DFLT,
+			STDERR_FILENO), 2);
 	return (0);
+}
+
+t_program	init_julia(char **v)
+{
+	t_program self;
+
+	self.mlx = mlx_init();
+	if (!self.mlx)
+	{
+		ft_putendl_fd(RED"fractol: skill issue."DFLT, STDERR_FILENO);
+		exit(2);
+	}
+	self.win = mlx_new_window(self.mlx, WIN_HEIGHT, WIN_WIDTH, "Julia Set");
+	if (!self.win)
+	{
+		ft_putendl_fd(RED"fractol: skill issue."DFLT, STDERR_FILENO);
+		exit(2);
+	}
+	// Remember to check for existence of those bounds.
+	self.range = (t_range){.left_bound = ft_atoi(v[2]),
+		ft_atoi(v[3])};
+	return (self);
+}
+
+int	parse_input(int c, char **v)
+{
+	if (c < 2)
+		return (NONE);
+	if (!ft_strncmp("JULIA", v[1], 6)
+		|| !ft_strncmp("julia", v[1], 6))
+		return (JULIA);
+	else if (!ft_strncmp("MANDELBROT", v[1], 11)
+		|| !ft_strncmp("mandelbrot", v[1], 11))
+		return (MANDELBROT);
+	return (NONE);
 }
