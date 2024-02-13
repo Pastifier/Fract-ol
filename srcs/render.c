a1aacde21a6a2a01c075639077e20564b4fd9fc7
Emran <therealemran.design@gmail.com>
2024-02-13 16:38:21 +0400

    Cache-locality optimisation (which doesn't do much tbh lol)

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 23:30:52 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/02/12 14:24:27 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/02/13 16:36:36 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	render(t_program *fractol)
 {
 	unsigned int	color;
 	int				escape_time;
+	int				i;
 	int				(*chosen_set)(int, t_program*, t_complex*);
 
 	if (JULIA == fractol->chosen_set)
@@ -24,18 +25,15 @@ void	render(t_program *fractol)
 		chosen_set = escape_time_mandel;
 	else
 		chosen_set = escape_time_julia;
-	fractol->pixel.y = -1;
-	while (++fractol->pixel.y < WIN_HEIGHT - 1)
+	i = -1;
+	while (++i < WIN_HEIGHT * WIN_WIDTH)
 	{
-		fractol->pixel.x = -1;
-		while (++fractol->pixel.x < WIN_WIDTH - 1)
-		{
-			escape_time = (*chosen_set)(MAX_ITER, fractol, &fractol->mouse_pos);
-			if (MAX_ITER == escape_time)
-				color = 0;
-			else
-				color = escape_time * fractol->algo_toggle;
-			pixel_put(&fractol->img, fractol->pixel, color % 0xFFFFFFFF);
-		}
+		fractol->pixel = (t_point){i % WIN_WIDTH, i / WIN_HEIGHT};
+		escape_time = (*chosen_set)(MAX_ITER, fractol, &fractol->mouse_pos);
+		if (MAX_ITER == escape_time)
+			color = 0;
+		else
+			color = escape_time * fractol->algo_toggle;
+		pixel_put(&fractol->img, fractol->pixel, color % 0xFFFFFFFF);
 	}
 }
